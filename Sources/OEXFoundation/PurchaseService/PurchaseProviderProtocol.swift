e64240b3a77c7bb252e52550cb5b2aa813882d4f
IvanStepanok <ivan.stepanok@raccoongang.com>
2025-09-05 13:21:48 +0300

    feat: add PurchaseProviderProtocol

---
+++ Sources/OEXFoundation/PurchaseService/PurchaseProviderProtocol.swift
@@ -0,0 +1,47 @@
+//
+//  PurchaseProviderProtocol.swift
+//  OEXFoundation
+//
+//  Created by Ivan Stepanok on 05.09.2025.
+//
+
+import Foundation
+
+public protocol PurchaseProviderProtocol: Sendable {
+    
+    // MARK: Configure
+    func configure(configuration: Any?)
+    
+    // MARK: CourseDetails Screen
+    func purchaseCourse(courseID: String, priceTier: String) async throws -> Bool
+    func getCoursePriceTier(courseID: String) async throws -> String?
+    func localizedPrice(tier: String) async throws -> String
+    
+    // MARK: Discovery Screen
+    func getLocalizedCoursesTiers(courseIDs: [String]) async throws -> [String: String]
+}
+
+#if DEBUG
+public class InAppPurchasesManagerMock: PurchaseProviderProtocol, @unchecked Sendable {
+    
+    public init() {}
+    
+    public func configure(configuration: Any?) {}
+    
+    public func getLocalizedCoursesTiers(courseIDs: [String]) async throws -> [String: String] {
+        [:]
+    }
+
+    public func purchaseCourse(courseID: String, priceTier: String) async throws -> Bool {
+        return true
+    }
+    
+    public func getCoursePriceTier(courseID: String) async throws -> String? {
+        return nil
+    }
+    
+    public func localizedPrice(tier: String) async throws -> String {
+        ""
+    }
+}
+#endif
